bd09884c8d6f657a5c43b4ceca0d2ac96b300566
sineflex <github@pstar7.rf.gd>
2018-10-13 17:08:29 +0200

    Add files via upload

--- exacore.js
+++ exacore.js
@@ -1,5 +1,5 @@
 const electron = require('electron')
-const path = require('path')
+
 const {app, document, mainWindow, ipcMain, BrowserWindow, setTitle, title} = require('electron')
   // Keep a global reference of the window object, if you don't, the window will
   // be closed automatically when the JavaScript object is garbage collected.
