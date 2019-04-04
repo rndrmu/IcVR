d7cacabbb8a2c9476e07bd7d01438f766b94290b
Yeetin' Pete <github@pstar7.rf.gd>
2019-04-04 19:17:22 +0200

    Add files via upload

---
+++ core.js
@@ -0,0 +1,17 @@
+var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
+
+var mainWindow = null;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+app.on('ready', function() {  
+  mainWindow = new BrowserWindow({width: 1660, height: 900, frame: false, icon: `./icon.ico`, title: ''});  
+  mainWindow.loadFile('./assets/app.html');  
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });  
+});
